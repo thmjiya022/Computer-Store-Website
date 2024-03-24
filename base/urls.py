342dde4c48f611d8adf0a023e26021da1e1e9fdc
Thobani <mjiyakhothobani@gmail.com>
2024-03-25 01:43:30 +0200

    login profile

--- base/urls.py
+++ base/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login/', views.login_page, name='login'),
+    path('logout/', views.login_page, name='logout'),
+    path('register/', views.register_user, name='register'),
+
     path('', views.home, name='home')
 ]
--- base/views.py
+++ base/views.py
@@ -1,5 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login,logout
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from . forms import UserForm
 
 # Create your views here.
+
+def login_page(request):
+    page = 'login'
+
+    if request.user.is_authenticated:
+        return redirect('home')
+    
+    if request.method == 'POST':
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password')
+
+        try:
+            User.objects.get(username=username)
+        except:
+            messages.error(request, 'user does not exist')
+        user = authenticate(request,username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'username or password does not exist')
+    
+    return render(request, 'base/login_register.html', {'page': page})
+
+def register_user(request):
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occured during registration')
+        
+    return render(request, 'base/login_register.html', {'form':form})
+
+
 def home(request):
     return render(request, 'base/home.html')
+
